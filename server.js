ee9dafbeb8835ab3f1d2d808af411a124ad635d2
ViniciusZinAlves <60885049+ViniciusZinAlves@users.noreply.github.com>
2024-11-19 13:57:52 -0300

    Aula 2 - Criando e Estruturando sua Primeira API com GET e Banco de Dados

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-import express from "express"
+import express from "express";
 
-const app = express()
-app.listen(3000,()=>{
-    console.log("Server ativo")
-})
+const app = express();
+app.use(express.json());
 
+const posts = [
+    { id: 1, descricao: "Gato fazendo a barba", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+];
 
-app.get("/api", (req, res) => {
-    res. status(200).send("Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
+app.listen(3000, () => {
+    console.log("Servidor escutando em http://localhost:3000.");
+});
+
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+    const postID = Number(req.params.id);
+    if (isNaN(postID)) return res.status(400).json({ error: "ID inválido." });
+
+    const post = posts.find(p => p.id === postID);
+    if (!post) return res.status(404).json({ error: "Post não encontrado." });
+    
+    res.status(200).json(post);
 });
